79b15220cb1c7220712a9107c852dadf22e7600f
hyewonrhee <hyewonrheee@gmail.com>
2023-11-28 16:51:07 -0800

    edited grade.sh

--- grade.sh
+++ grade.sh
@@ -5,12 +5,45 @@ rm -rf grading-area
 
 mkdir grading-area
 
+
 git clone $1 student-submission
 echo 'Finished cloning'
+cd student-submission
 
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+if [[ -f ListExamples.java ]]
+then 
+    echo 'Found ListExamples.java'
+else
+    echo 'Need ListExamples.java'
+    exit 1
+fi
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+cd ..
+
+
+cp student-submission/*.java *.java grading-area
+cp -r lib grading-area
+echo "grading-area contents:"
+ls grading-area
+
+
+javac -cp $CPATH grading-area/*.java 2> error-output.txt
+if [ $? -ne 0 ]
+then 
+    echo 'Could not compile'
+else
+    echo 'Compiled successfully'
+fi
+
+
+cd grading-area
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test-output.txt
+cat test-output.txt
+
+if grep "OK" test-output.txt
+then
+    echo "PASSED ALL TESTS!"
+else
+    echo "YOU FAILED AT LEAST ONE TEST!"
+fi
